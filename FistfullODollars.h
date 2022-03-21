9b05ec9a204be7afb576ed85ad3baf0576ef9831
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:40:59 +1030

    Update FistfullODollars.h

--- FistfullODollars.h
+++ FistfullODollars.h
@@ -3,16 +3,4 @@
 #include <string>
 #include "Player.h"
 
-class FistfullODollars : public Player {
-   public:
-       FistfullODollars();
-       std::string performMove();
-       void setCtr(int);
-       ~FistfullODollars();
-
-   private:
-       std::string hand;
-       int ctr;
-};
-
 #endif
