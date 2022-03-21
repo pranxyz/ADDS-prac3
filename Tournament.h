3b5e0bb95a0f0009dff3842273edfbe5e7ad0e04
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:47:17 +1030

    Update Tournament.h

--- Tournament.h
+++ Tournament.h
@@ -9,16 +9,6 @@ class Tournament {
    
 Player * run(array<Player *, 8>  competitors)
 
-       Tournament();
-       void addPlayers(Player*);
-       std::string compete();
-       ~Tournament();
-
-   private:
-       std::vector<Player*> Round1;
-       std::vector<Player*> Round2;
-       std::vector<Player*> Round3;
-
 };
 
 #endif
