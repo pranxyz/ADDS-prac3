6940225780e891459b7877ed49bd51b9933cf670
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:37:41 +1030

    Update Avalanche.cpp

--- Avalanche.cpp
+++ Avalanche.cpp
@@ -4,13 +4,3 @@
 #include "Avalanche.h"
 
 using namespace std;
-
-Avalanche::Avalanche() : Player("Avalanche") {}
-
-Avalanche::~Avalanche() {}
-
-string Avalanche::performMove()
-{
-   hand = "Rock"; // Always picks rock
-   return hand;
-}
