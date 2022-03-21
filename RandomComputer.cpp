aa0a1915c135aec9a8128cefd999f6e9f9f8fc04
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:43:38 +1030

    Update RandomComputer.cpp

--- RandomComputer.cpp
+++ RandomComputer.cpp
@@ -4,27 +4,3 @@
 #include <iostream> //remove this
 #include <cstdlib>
 using namespace std;
-
-RandomComputer::RandomComputer() : Player("RandomComputer"){
-
-}
-RandomComputer::~RandomComputer(){
-}
-
-void RandomComputer::addAnswer(string ans)
-{
-userAnswers.push_back(ans);
-}
-string RandomComputer::performMove()
-{
-// returns a psuedo random number between 1 and 3
-int aiMove = rand() % 3 + 1;
-
-if (aiMove == 1) {
-return "Rock";
-} else if (aiMove == 2) {
-return "Paper";
-} else {
-return "Scissors";
-}
-}
