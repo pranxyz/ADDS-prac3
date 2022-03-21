53a5783138852d22cabe96eaacab13579c0a1b02
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:38:47 +1030

    Update Crescendo.cpp

--- Crescendo.cpp
+++ Crescendo.cpp
@@ -4,31 +4,3 @@
 #include "Crescendo.h"
 
 using namespace std;
-
-Crescendo::Crescendo() : Player("Crescendo") {
-   ctr = 0;
-}
-
-Crescendo::~Crescendo() {}
-
-void Crescendo::setCtr(int newCtr)
-{
-   ctr = newCtr; // This ctr will be reset so the AI is 'reset' each round
-}
-
-string Crescendo::performMove()
-{
-   if (ctr == 0)
-   {
-       return "Paper";
-   }
-   else if (ctr == 1)
-   {
-       return "Scissors";
-   }
-   else if (ctr == 2)
-   {
-       return "Rock";
-   }
-   return 0; // Remove Warning Flag
-}
