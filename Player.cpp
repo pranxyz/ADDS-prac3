2c974836d3840ffca2fa6b77ff81a75ad25463b1
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:42:52 +1030

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -3,34 +3,3 @@
 #include "Player.h"
 
 using namespace std;
-
-Player::Player(){
-}
-
-Player::Player(string name2)
-{
-   name = name2;
-}
-
-Player::~Player(){
-}
-
-string Player::getName()
-{
-   return name;
-}
-
-void Player::setCtr(int ctr)
-{
-
-}
-
-void Player::addAnswer(string ans)
-{
-
-}
-
-string Player::performMove()
-{
-   return "";
-}
