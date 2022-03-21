c3a99bbcc3f1ecc7fd7abfdb792366bae1affed5
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:43:15 +1030

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,19 +2,4 @@
 #define _PLAYER_H
 #include <string>
 
-class Player {
-   public:
-       Player();
-       Player(std::string);
-       std::string getName();
-       virtual std::string performMove();
-       virtual void setCtr(int); // Used only in crescendo fisftfull and paperdoll
-       virtual void addAnswer(std::string); // used only in AdvancedComputer
-       ~Player();
-
-   private:
-       std::string name;
-
-};
-
 #endif
