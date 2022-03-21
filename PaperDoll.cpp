6db9aa21cca9d5bcbe66883d7d8ba291f954f23b
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:42:03 +1030

    Update PaperDoll.cpp

--- PaperDoll.cpp
+++ PaperDoll.cpp
@@ -4,32 +4,3 @@
 #include "PaperDoll.h"
 
 using namespace std;
-
-PaperDoll::PaperDoll() : Player("PaperDoll") {
-   ctr = 0;
-}
-
-PaperDoll::~PaperDoll() {}
-
-void PaperDoll::setCtr(int newCtr)
-{
-   ctr = newCtr;
-}
-
-
-string PaperDoll::performMove()
-{
-   if (ctr == 0)
-   {
-       return "Paper";
-   }
-   else if (ctr == 1)
-   {
-       return "Scissors";
-   }
-   else if (ctr == 2)
-   {
-       return "Scissors";
-   }
-   return 0; // Remove Warning Flag
-}
