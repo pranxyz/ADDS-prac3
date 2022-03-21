10af226e4c54f2fda1b168c909432d95caf1caa6
pranxyz <65145297+pranxyz@users.noreply.github.com>
2022-03-21 20:43:54 +1030

    Update RandomComputer.h

--- RandomComputer.h
+++ RandomComputer.h
@@ -4,16 +4,6 @@
 #include "Player.h"
 #include <vector>
 
-class RandomComputer : public Player {
-public:
-RandomComputer();
-std::string performMove();
-void addAnswer(std::string ans);
-~RandomComputer();
-
-private:
-int handInt;
-std::vector<std::string> userAnswers;
 
 };
 #endif
